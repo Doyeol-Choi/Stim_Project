1cf934a12dd9c037c32a2bae4b4a92291d9a1a5a
Mamwalu8 <105907915+Mamwalu8@users.noreply.github.com>
2022-07-21 13:16:32 +0900

    Update GameVO.java

--- stim/src/main/java/com/stim/vo/GameVO.java
+++ stim/src/main/java/com/stim/vo/GameVO.java
@@ -1,17 +1,25 @@
 package com.stim.vo;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class GameVO {
 
-	private int code;
-	private String name;
-	private int price;
-	private String releaseDate;
-	private String developer;
-	private String distribution;
-	private String context;
-	private int sale;
-	
+	private int game_code;
+	private String game_name;
+	private int game_price;
+	private Date game_releaseDate;
+	private String game_developer;
+	private String game_distribution;
+	private String game_context;
+	private int game_salesRate;
+
+
+
+
+
+
+
 }
