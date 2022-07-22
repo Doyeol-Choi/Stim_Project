12501cfd848a0519d43b7acea083d8ead8cb0723
Doyeol-Choi <karias8863@gmail.com>
2022-07-22 22:26:44 +0900

    1차 머지후 수정

--- stim/src/main/java/com/stim/config/SecurityConfig.java
+++ stim/src/main/java/com/stim/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -22,7 +21,7 @@ public class SecurityConfig{
     }
 	
 	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 //				.antMatchers("/user/**").authenticated()     
@@ -58,8 +57,6 @@ public class SecurityConfig{
 	
 	// CORS 허용 적용
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-         return null;
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
