f71731c738f8f7918741f7b0c0e40579169abaaa
Doyeol-Choi <karias8863@gmail.com>
2022-07-27 00:03:19 +0900

    아이디 기억 쿠키 추가

--- stim/src/main/resources/static/js/login.js
+++ stim/src/main/resources/static/js/login.js
@@ -2,20 +2,29 @@ $(document).ready(function(){
 	// 저장된 쿠키값을 가져와서 ID 칸에 넣어준다. 없으면 공백으로 들어감.
     var key = getCookie("key");
     $("#user_id").val(key); 
-     
+    if(document.cookie.indexOf("key") != -1) {
+		renewalCookie("key")
+	} 
     // 그 전에 ID를 저장해서 처음 페이지 로딩 시, 입력 칸에 저장된 ID가 표시된 상태라면,
     if($("#user_id").val() != ""){ 
-        $("#checkId").attr("checked", true); // ID 저장하기를 체크 상태로 두기.
+        $("#rememberId").attr("checked", true); // ID 저장하기를 체크 상태로 두기.
     }
+    
+    $("#rememberId").change(function(){ // 체크박스에 변화가 있다면,
+        if($("#rememberId").is(":checked")){ // ID 저장하기 체크했을 때,
+            setCookie("key", $("#user_id").val(), 7); // 7일 동안 쿠키 보관
+        }else{ // ID 저장하기 체크 해제 시,
+            deleteCookie("key");
+        }
+    });
+     
+    // ID 저장하기를 체크한 상태에서 ID를 입력하는 경우, 이럴 때도 쿠키 저장.
+    $("#user_id").keyup(function(){ // ID 입력 칸에 ID를 입력할 때,
+        if($("#rememberId").is(":checked")){ // ID 저장하기를 체크한 상태라면,
+            setCookie("key", $("#user_id").val(), 7); // 7일 동안 쿠키 보관
+        }
+    });
 })
-
-function rememberId(cookieName) {
-	if($("#rememberId").is(":checked")) {
-		renewalCookie(cookieName)
-	} else if (cookieData.indexOf(cookieName) != -1) {
-		deleteCookie(cookieName)
-	}
-}
 
 // 쿠키 저장하기 
 // setCookie => saveid함수에서 넘겨준 시간이 현재시간과 비교해서 쿠키를 생성하고 지워주는 역할
