27b45ec1b222d2e2d41f0f6869dd6abfa2b8a460
Doyeol-Choi <karias8863@gmail.com>
2022-08-05 01:34:07 +0900

    게임 상세페이지 수정중

--- stim/src/main/resources/static/js/game/gameview.js
+++ stim/src/main/resources/static/js/game/gameview.js
@@ -82,38 +82,41 @@ function inputReply(){
 	let user_code = $("#user_code").val();
 	let game_code = $("#game_code").val();
 	let grade_context = $("#grade_context").val();
-	let grade_rate = $("#grade_rate").val();
-		
-	$.ajax({
-		url : "/gamereply",
-		data : {
-			"user_code":user_code,
-			"game_code":game_code,
-			"grade_context":grade_context,
-			"grade_rate":grade_rate
-		},
-		type : "POST",
-		success : function(data) {
-			html = "<div class='reply_list' id='mentListBox"+data.grade_code+"'>";
-			html += "<div class='reply_image'>";
-			html += "<a> <img alt='프로필 사진' src='/image/profile/" + data.user_picture+"'> </a>";
-			html += "</div>";
-			html += "<div class='grade_content'>";
-			html += "<div class='user_nickname' >"+data.user_nickname+"</div>";
-			html += "<span class='grade_regdate' >"+dateFormat()+"</span>";
-			html += "<div class='grade_context'>"+data.grade_context+"</div>";
-			html += "<div class='grade_rate'>"+data.grade_rate+"</div>";
-			html += "<button class='grade_deletebtn' th:if='${"+data.user_code+" == #authentication.principal.user_code}' onclick='deleteReply("+data.grade_code+")' >삭제</button>";
-			html += "</div>";
-			html += "</div>";
-			
-			$("#reply_lists").prepend(html);
-		},
-		error : function(){
-			alert('추가 실패');
-		}	
-	});
-	$("#reply_text").val("");
+	let grade_rate = $('input[name="grade_rate"]:checked').val();
+	
+	if(grade_rate != null) {
+		$.ajax({
+			url : "/gamereply",
+			data : {
+				"user_code":user_code,
+				"game_code":game_code,
+				"grade_context":grade_context,
+				"grade_rate":grade_rate
+			},
+			type : "POST",
+			success : function(data) {
+				html = "<div class='reply_list' id='mentListBox"+data.grade_code+"'>";
+				html += "<a href='/profile/'"+user_code+"><img id='reply_picture' alt='프로필 사진' src='/image/profile/"+data.user_picture+"'></a>";
+				html += "<div class='grade_content'>";
+				html += "<span class='user_nickname' >"+data.user_nickname+"</span>";
+				html += "<span class='grade_regdate' >"+dateFormat()+"</span>";
+				html += "<span id=''grade_rateG' th:if='${#strings.equals("+data.grade_rate+", \'g\')}'>Good</span>";
+				html += "<span id=''grade_rateB' th:if='${#strings.equals("+data.grade_rate+", \'b\')}'>Bad</span>";
+				html += "<div class='grade_context'>"+data.grade_context+"</div>";
+				html += "<button class='grade_deletebtn' th:if='${"+data.user_code+" == #authentication.principal.user_code}' onclick='deleteReply("+data.grade_code+")' >삭제</button>";
+				html += "</div>";
+				html += "</div>";
+				
+				$("#reply_lists").prepend(html);
+			},
+			error : function(){
+				alert('추가 실패');
+			}	
+		});
+		$("#reply_text").val("");	
+	} else {
+		alert("평점을 체크해주세요.");
+	}
 
 }
 
